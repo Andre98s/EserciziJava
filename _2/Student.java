d59dc9a1b38c1d57c9b1bbfbef6f4af1f55bd914
Andre98s <126459071+Andre98s@users.noreply.github.com>
2023-03-22 17:17:57 +0100

    Add files via upload
    Constructors

---
+++ _2/Student.java
@@ -0,0 +1,28 @@
+package javaoop._2;
+
+public class Student {
+    public String name;
+
+    public int age;
+
+
+    public Student(String name, int age){
+        System.out.println("Constructing!");
+
+        this.age = age;
+
+        this.name = name;
+        System.out.println("Constructed!");
+
+    }
+
+    public Student(String name){
+        this.name=name;
+    }
+    public Student(int age){
+        this.age=age;
+    }
+    public Student(){
+
+    }
+}
